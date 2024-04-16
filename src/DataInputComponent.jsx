2b8cd48c6eb0f4396c75b750619d223c094cbeeb
atsayu <quyenhoangntt03@gmail.com>
2024-04-16 22:36:35 +0700

    feat: represent user data in table

--- src/DataInputComponent.jsx
+++ src/DataInputComponent.jsx
@@ -1,8 +1,13 @@
 
 import { InputComponent } from './components/InputComponent'
 import { Button, Input } from '@mui/material';
-
-
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 
 export default function DataInputComponent({ data, setNewData, variables, updateStoredData, storedData }) {
@@ -36,7 +41,7 @@ export default function DataInputComponent({ data, setNewData, variables, update
           )
         })
       }
-      {
+      {/* {
         storedData &&
           storedData.map((userData, index) => {
             return (
@@ -45,8 +50,38 @@ export default function DataInputComponent({ data, setNewData, variables, update
               </div>
             )
           })
+      } */}
+      {
+        storedData &&
+        <>
+        <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              {
+                variables.map((variable, index) => {
+                  return <TableCell key={index}>{variable}</TableCell>
+                })
+              }
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+              storedData.map((userData, index) => {
+                return <TableRow key={index}>
+                  {
+                    variables.map((variable, index) => {
+                      return <TableCell key={index}>{userData[variable]}</TableCell>
+                    })
+                  }
+                </TableRow>
+              })
+            }
+          
+          </TableBody>
+        </Table>
+      </TableContainer></>
       }
-
       <Button size="small" variant='contained' onClick={storeData}>ADD NEW Data</Button>
     </div>
 
